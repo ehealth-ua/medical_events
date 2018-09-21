84cec01761ae211f677eb20cff4d59bd2395c045
Alex Kovalevych <AlexKovalevych@users.noreply.github.com>
2018-09-21 12:17:42 +0300

    fix create observation (#71)

--- apps/core/lib/core/observations/observation.ex
+++ apps/core/lib/core/observations/observation.ex
@@ -80,6 +80,9 @@ defmodule Core.Observation do
         {"id", v} ->
           {:_id, v}
 
+        {"based_on", nil} ->
+          {:based_on, nil}
+
         {"based_on", v} ->
           {:based_on, Enum.map(v, &Reference.create/1)}
 
