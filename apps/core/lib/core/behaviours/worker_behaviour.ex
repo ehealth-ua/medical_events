1ffb978d006ba57607c6bc54787328a96b7db399
Alex Kovalevych <AlexKovalevych@users.noreply.github.com>
2018-12-13 14:44:21 +0200

    fix rpc worker behaviour (#228)

--- apps/core/lib/core/behaviours/worker_behaviour.ex
+++ apps/core/lib/core/behaviours/worker_behaviour.ex
@@ -1,5 +1,5 @@
 defmodule Core.Behaviours.WorkerBehaviour do
   @moduledoc false
 
-  @callback run(module :: atom, function :: atom, args :: list(), attempt :: integer) :: any()
+  @callback run(basename :: binary, module :: atom, function :: atom, args :: list()) :: any()
 end
