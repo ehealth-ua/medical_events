c7718eb8a414b0c8ad9b8850a57adfe9ca913fe3
Maryna <marinakrasnovatr81@gmail.com>
2019-01-23 13:22:31 +0200

    chore: release version 1.0.1

--- mix.exs
+++ mix.exs
@@ -1,8 +1,10 @@
 defmodule MedicalEvents.Umbrella.Mixfile do
   use Mix.Project
 
+  @version "1.0.1"
   def project do
     [
+      version: @version,
       apps_path: "apps",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
@@ -28,7 +30,8 @@ defmodule MedicalEvents.Umbrella.Mixfile do
     [
       {:distillery, "~> 2.0", runtime: false, override: true},
       {:excoveralls, "~> 0.9.1", only: [:dev, :test]},
-      {:credo, "~> 0.9.3", only: [:dev, :test]}
+      {:credo, "~> 0.9.3", only: [:dev, :test]},
+      {:git_ops, "~> 0.6.0", only: [:dev]}
     ]
   end
 end
