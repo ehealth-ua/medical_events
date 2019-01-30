164dc13aa6f28a32b30869a250917511dfc739fa
Marina Krasnova <marinakrasnovatr81@gmail.com>
2019-01-30 10:47:42 +0200

    Git ops (#255)

---
+++ CHANGELOG.md
@@ -0,0 +1,11 @@
+# Change Log
+
+All notable changes to this project will be documented in this file.
+See [Conventional Commits](Https://conventionalcommits.org) for commit guidelines.
+
+<!-- changelog -->
+
+## [1.0.1](https://github.com/edenlabllc/medical_events/compare/1.0.1...1.0.1) (2019-1-23)
+
+
+
--- config/config.exs
+++ config/config.exs
@@ -9,6 +9,29 @@ use Mix.Config
 # back to each application for organization purposes.
 import_config "../apps/*/config/config.exs"
 
+config :git_ops,
+  mix_project: MedicalEvents.Umbrella.Mixfile,
+  changelog_file: "CHANGELOG.md",
+  repository_url: "https://github.com/edenlabllc/medical_events",
+  types: [
+    # Makes an allowed commit type called `tidbit` that is not
+    # shown in the changelog
+    tidbit: [
+      hidden?: true
+    ],
+    # Makes an allowed commit type called `important` that gets
+    # a section in the changelog with the header "Important Changes"
+    important: [
+      header: "Important Changes"
+    ]
+  ],
+  # Instructs the tool to manage your mix version in your `mix.exs` file
+  # See below for more information
+  manage_mix_version?: true,
+  # Instructs the tool to manage the version in your README.md
+  # Pass in `true` to use `"README.md"` or a string to customize
+  manage_readme_version: "README.md"
+
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
 import_config "#{Mix.env()}.exs"
