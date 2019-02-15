4ae37ffb37cfb1db5d5a72a5bc7ab20c6b47bf24
Alex Kovalevych <AlexKovalevych@users.noreply.github.com>
2019-02-15 14:05:23 +0200

    feat: update job via ergonode (#314)

---
+++ apps/core/lib/core/mongo/transaction.ex
@@ -0,0 +1,44 @@
+defmodule Core.Mongo.Transaction do
+  @moduledoc false
+
+  @worker Application.get_env(:core, :rpc_worker)
+
+  defstruct operations: []
+
+  def add_operation(%__MODULE__{} = transaction, collection, :insert, value) do
+    value_bson =
+      value
+      |> BSON.Encoder.encode()
+      |> do_bson_encode("")
+      |> Base.encode64()
+
+    operation = %{"set" => value_bson, "operation" => "insert", "collection" => collection}
+    %{transaction | operations: transaction.operations ++ [operation]}
+  end
+
+  def add_operation(%__MODULE__{} = transaction, collection, :update, filter, set) do
+    filter_bson =
+      filter
+      |> BSON.Encoder.encode()
+      |> do_bson_encode("")
+      |> Base.encode64()
+
+    set_bson =
+      set
+      |> BSON.Encoder.encode()
+      |> do_bson_encode("")
+      |> Base.encode64()
+
+    operation = %{"filter" => filter_bson, "set" => set_bson, "operation" => "update_one", "collection" => collection}
+    %{transaction | operations: transaction.operations ++ [operation]}
+  end
+
+  def flush(%__MODULE__{} = transaction) do
+    @worker.run("me_transactions", Core, :transaction, Jason.encode!(transaction.operations))
+  end
+
+  defp do_bson_encode(value, acc) when is_binary(value), do: acc <> value
+  defp do_bson_encode(value, acc) when is_integer(value), do: acc <> <<value>>
+  defp do_bson_encode([h | tail], acc), do: acc <> do_bson_encode(h, acc) <> do_bson_encode(tail, "")
+  defp do_bson_encode([], acc), do: acc
+end
