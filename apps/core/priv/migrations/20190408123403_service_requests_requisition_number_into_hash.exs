80fa7462597d2b2e313345b90b1571b30a298c7d
imax-iva <34630609+imax-iva@users.noreply.github.com>
2019-04-09 13:00:54 +0300

    fix: service requests requisition mumber into hash migration improved (#394)

--- apps/core/priv/migrations/20190408123403_service_requests_requisition_number_into_hash.exs
+++ apps/core/priv/migrations/20190408123403_service_requests_requisition_number_into_hash.exs
@@ -5,24 +5,13 @@ defmodule Core.Migrations.ServiceRequestsRequisitionNumberIntoHash do
   alias Core.Mongo
   alias Core.Mongo.Transaction
   alias Core.ServiceRequest
-  require Logger
 
   @collection ServiceRequest.metadata().collection
 
   def change do
-    {successful_count, failed_count} =
-      @collection
-      |> Mongo.find(%{"requisition" => %{"$exists" => true}}, projection: %{"_id" => true, "requisition" => true})
-      |> Enum.reduce({0, 0}, fn service_request, {successful_acc, failed_acc} ->
-        {successful, failed} = update_service_request(service_request)
-        {successful_acc + successful, failed_acc + failed}
-      end)
-
-    Logger.info(
-      "Service request requisition number convertation process finished: successful - #{successful_count}, failed - #{
-        failed_count
-      }"
-    )
+    @collection
+    |> Mongo.find(%{"requisition" => %{"$exists" => true}}, projection: %{"_id" => true, "requisition" => true})
+    |> Enum.each(&update_service_request/1)
   end
 
   defp update_service_request(%{"_id" => id, "requisition" => requisition}) do
@@ -35,11 +24,10 @@ defmodule Core.Migrations.ServiceRequestsRequisitionNumberIntoHash do
 
     case result do
       :ok ->
-        {1, 0}
+        :ok
 
       {:error, reason} ->
-        Logger.error("Failed to update service request (id: #{id}): #{inspect(reason)}")
-        {0, 1}
+        raise "Failed to process migration: #{inspect(reason)}"
     end
   end
 end
