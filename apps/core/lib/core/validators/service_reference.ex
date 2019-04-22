3052e1b22b97dfa149226c8777818dccd72fb992
Maxim Batrak <43367461+BatrakM@users.noreply.github.com>
2019-04-22 16:15:14 +0300

    feat: validate that service/group is active and request_allowed = true (#413)

--- apps/core/lib/core/validators/service_reference.ex
+++ apps/core/lib/core/validators/service_reference.ex
@@ -9,6 +9,12 @@ defmodule Core.Validators.ServiceReference do
     category = Keyword.get(options, :category)
 
     case @worker.run("ehealth", EHealth.Rpc, :service_by_id, [to_string(service_id)]) do
+      {:ok, %{is_active: false}} ->
+        {:error, message(options, "Service should be active")}
+
+      {:ok, %{request_allowed: false}} ->
+        {:error, message(options, "Request is not allowed for the service")}
+
       {:ok, service} ->
         check_category(service, category, options)
 
