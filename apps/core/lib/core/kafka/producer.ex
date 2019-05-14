5bee43d8eb6b6ac60c82db90b5040f29c0e2da47
Maxim Batrak <43367461+BatrakM@users.noreply.github.com>
2019-05-14 12:22:46 +0300

    fix: kafka producer partition selection (#428)

--- apps/core/lib/core/kafka/producer.ex
+++ apps/core/lib/core/kafka/producer.ex
@@ -14,17 +14,15 @@ defmodule Core.Kafka.Producer do
     do: Kaffe.Producer.produce_sync(@event_manager_topic, 0, "", :erlang.term_to_binary(event))
 
   def publish_medical_event(request) do
-    partition = get_partition(request.patient_id, @medical_events_topic)
     key = get_key(request.patient_id)
-    Logger.info("Publishing kafka event to topic: #{@medical_events_topic}, partition: #{partition}, key: #{key}")
-    Kaffe.Producer.produce_sync(@medical_events_topic, partition, key, :erlang.term_to_binary(request))
+    Logger.info("Publishing kafka event to topic: #{@medical_events_topic}, key: #{key}")
+    Kaffe.Producer.produce_sync(@medical_events_topic, key, :erlang.term_to_binary(request))
   end
 
   def publish_mongo_event(%Event{} = event) do
-    partition = get_partition(event.actor_id, @mongo_events_topic)
     key = get_key(event.actor_id)
-    Logger.info("Publishing kafka event to topic: #{@mongo_events_topic}, partition: #{partition}, key: #{key}")
-    Kaffe.Producer.produce_sync(@mongo_events_topic, partition, key, :erlang.term_to_binary(event))
+    Logger.info("Publishing kafka event to topic: #{@mongo_events_topic}, key: #{key}")
+    Kaffe.Producer.produce_sync(@mongo_events_topic, key, :erlang.term_to_binary(event))
   end
 
   defp get_key(%BSON.Binary{binary: id}) do
@@ -32,17 +30,4 @@ defmodule Core.Kafka.Producer do
   end
 
   defp get_key(value), do: value
-
-  defp get_partition(nil, _), do: 0
-  defp get_partition("", _), do: 0
-
-  defp get_partition(%BSON.Binary{binary: id}, topic) do
-    get_partition(UUID.binary_to_string!(id), topic)
-  end
-
-  defp get_partition(patient_id, topic) do
-    partitions_number = Confex.fetch_env!(:core, :kafka)[:partitions][topic] - 1
-    {i, _} = Integer.parse(String.first(patient_id), 16)
-    trunc(i * partitions_number / 12)
-  end
 end
