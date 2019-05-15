0d7c271fd378c28af047386c4cc79f7a726dbcfb
Maxim Batrak <43367461+BatrakM@users.noreply.github.com>
2019-05-15 21:02:44 +0300

    feat: you can now use short json schema in create encounter package (#435)

--- apps/core/lib/core/validators/json_schema.ex
+++ apps/core/lib/core/validators/json_schema.ex
@@ -9,6 +9,7 @@ defmodule Core.Validators.JsonSchema do
 
   use_schema(:package_create, "json_schemas/packages/package_create.json")
   use_schema(:package_create_signed_content, "json_schemas/packages/package_create_signed_content.json")
+  use_schema(:package_create_signed_content_short, "json_schemas/packages/package_create_signed_content_short.json")
   use_schema(:package_cancel, "json_schemas/packages/package_cancel.json")
   use_schema(:package_cancel_signed_content, "json_schemas/packages/package_cancel_signed_content.json")
   use_schema(:diagnostic_report_package_create, "json_schemas/packages/diagnostic_report_package_create.json")
