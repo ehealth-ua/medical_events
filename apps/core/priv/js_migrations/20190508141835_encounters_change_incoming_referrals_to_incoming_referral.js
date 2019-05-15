e4af88d1ebf58e2102e1d61a324b9609ee1052bf
Maxim Batrak <43367461+BatrakM@users.noreply.github.com>
2019-05-15 22:00:41 +0300

    chore: encounters incoming referrals migration optimization (#434)

--- apps/core/priv/js_migrations/20190508141835_encounters_change_incoming_referrals_to_incoming_referral.js
+++ apps/core/priv/js_migrations/20190508141835_encounters_change_incoming_referrals_to_incoming_referral.js
@@ -1,13 +1,17 @@
 db.patients.find({"encounters": {$ne: {}}}).forEach((patient) => {
+  let save = false;
   Object.keys(patient.encounters).forEach((encounter_id) => {
     if (patient.encounters[encounter_id].incoming_referrals) {
       patient.encounters[encounter_id].incoming_referral = patient.encounters[encounter_id].incoming_referrals[0];
       delete patient.encounters[encounter_id].incoming_referrals;
-      db.patients.save(patient);
+      save = true;  
     }
-    else if (!patient.encounters[encounter_id].incoming_referral) {
+    else if (!patient.encounters[encounter_id].hasOwnProperty("incoming_referral")) {
       patient.encounters[encounter_id].incoming_referral = null;
-      db.patients.save(patient);
+      save = true;
     }   
   });
+  if (save) {
+    db.patients.save(patient);
+  }
 });
