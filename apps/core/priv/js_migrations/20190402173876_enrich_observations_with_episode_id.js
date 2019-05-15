5ff1d20f342c9b6f61ff6e7a691117f34889e897
Alex Kovalevych <AlexKovalevych@users.noreply.github.com>
2019-05-15 20:24:32 +0300

    fix: js migrations (#436)

--- apps/core/priv/js_migrations/20190402173876_enrich_observations_with_episode_id.js
+++ apps/core/priv/js_migrations/20190402173876_enrich_observations_with_episode_id.js
@@ -1,21 +1,13 @@
-db.observations.aggregate(
-  {
-    $lookup:
-    {
-      from: "patients",
-      let: { "entity_id": "$context.identifier.value" },
-      pipeline: [
-        { $project: { encounters: { $objectToArray: "$encounters" } } },
-        { $unwind: "$encounters" },
-        { $project: { encounters: "$encounters.v" } },
-        { $replaceRoot: { newRoot: "$encounters" } },
-        { $match: { $expr: { $eq: ["$id", "$$entity_id"] } } }
-      ],
-      as: "encounters"
+db.observations.find({ context_episode_id: { $exists: false } }).forEach(function (observation) {
+  var patient = db.patients.findOne({ _id: observation.patient_id });
+  for (var key in patient.encounters) {
+    if (UUID(key).hex() == observation.context.identifier.value.hex()) {
+      db.observations.updateOne(
+        { _id: observation._id },
+        {
+          $set: { context_episode_id: patient.encounters[key].episode.identifier.value }
+        });
+      return;
     }
-  },
-  { $unwind: "$encounters" },
-  { $project: { _id: 0, observation_id: "$_id", episode_id: "$encounters.episode.identifier.value" } }
-).forEach(function (thisDoc) {
-  db.observations.updateOne({ _id: thisDoc.observation_id }, { $set: { context_episode_id: thisDoc.episode_id } })
+  }
 });
