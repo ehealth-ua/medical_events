2e74976497d875ca4f54e96587434743e9e59d7f
Alex Kovalevych <AlexKovalevych@users.noreply.github.com>
2019-05-21 10:23:03 +0300

    feat: number generator save via ergonode (#448)

--- apps/number_generator/lib/number_generator/application.ex
+++ apps/number_generator/lib/number_generator/application.ex
@@ -6,6 +6,18 @@ defmodule NumberGenerator.Application do
   def start(_type, _args) do
     # List all child processes to be supervised
     children = []
+
+    children =
+      if Application.get_env(:number_generator, :env) == :prod do
+        children ++
+          [
+            {Cluster.Supervisor,
+             [Application.get_env(:number_generator, :topologies), [name: NumberGenerator.ClusterSupervisor]]}
+          ]
+      else
+        children
+      end
+
     opts = [strategy: :one_for_one, name: NumberGenerator.Supervisor]
     Supervisor.start_link(children, opts)
   end
