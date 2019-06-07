f5df3a4be50db2c9a6f93b58676b4a20ebb84371
Maxim Batrak <43367461+BatrakM@users.noreply.github.com>
2019-06-07 15:26:22 +0300

    feat: you can now configure consumer batch size (#478)

--- apps/person_consumer/config/prod.exs
+++ apps/person_consumer/config/prod.exs
@@ -5,5 +5,6 @@ config :person_consumer,
     endpoints: {:system, :string, "KAFKA_BROKERS"},
     topics: ["person_events"],
     consumer_group: "person_events_group",
-    message_handler: PersonConsumer.Kafka.PersonEventConsumer
+    message_handler: PersonConsumer.Kafka.PersonEventConsumer,
+    max_bytes: {:system, :integer, "PERSON_CONSUMER_BATCH_MAX_BYTES", 500_000}
   ]
